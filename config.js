d45a2b55b41dd04c92c6a1a5d66182171e7eb4d9
unluky3 <levaqa19@gmail.com>
2024-08-23 16:16:34 +0300

    added catppuccin theme

--- config.js
+++ config.js
@@ -12,13 +12,13 @@ const CONFIG = {
 
   
 	// General
-	name: 'John',
+	name: 'lev',
 	openInNewTab: true,
 	twelveHourFormat: false,
-  title: 'Bento',
+  title: 'HomePage',
 
 	// Theme
-	theme: 'bento',
+	theme: 'latte-mocha',
 	imageBackground: false,
   imageUrl: './assets/background.jpg', // Set custom background image URL. If the page is served insecurely, you may have issues loading images from pages over https.
 
